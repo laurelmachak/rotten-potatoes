d779fbea98b9b868146a52c1b9c7edd02032e079
laurelmachak <laurelmachak@gmail.com>
2017-10-06 11:04:57 -0700

    added remote listen and installed mocha for testing

--- app.js
+++ app.js
@@ -90,6 +90,6 @@ app.delete('/reviews/:id', function (req, res){
   });
 });
 
-app.listen(3000, function (){
+app.listen(process.env.PORT || 3000, function (){
   console.log('Portfolio App listening on port 3000!');
 });
